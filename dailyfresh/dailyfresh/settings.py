693f3509b05a0e3ed52650e2ab275d81e7ebd3fd
老王 <2304723721@qq.com>
2018-10-18 17:34:02 +0800

    商品

--- dailyfresh/dailyfresh/settings.py
+++ dailyfresh/dailyfresh/settings.py
@@ -165,4 +165,4 @@ DEFAULT_FILE_STORAGE="utils.fdfs.storage.FDFSStorage"
 FDFS_CLIENT_CONF="./utils/fdfs/client.conf"
 
 # 设置fdfs存储服务器上nginx的IP和端口号
-FDFS_URL="http://192.168.12.232:7777/"
+FDFS_URL="http://192.168.12.232:8888/"
--- dailyfresh/goods/urls.py
+++ dailyfresh/goods/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from goods.views import IndexView
+from django import views
 
 
 urlpatterns = [
